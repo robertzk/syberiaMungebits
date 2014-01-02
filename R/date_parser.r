35271012a16c07d7206241a34021ebc65bd95bd2
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-01-02 14:48:28 -0600

    date parser

--- R/date_parser.r
+++ R/date_parser.r
@@ -7,7 +7,18 @@
 #' @return numeric. Number of years
 #' @export
 #' @importFrom stringr str_extract
-date_parser <- function(col) {
-  as.numeric(str_extract(col, '^[0-9]+'))
+#' @examples
+#' dd <- data.frame(cbind(c("", "8 days", "20 days")), stringsAsFactors = FALSE)
+#' date_parser(dd, 1)
+#' print(dd[[1]])
+#' # [1] 0 8 20
+.date_parser <- function(col) {
+  print(col)
+  as.numeric(paste0("0",
+    substring(col, 1,
+      max(which(strsplit(col, '')[[1]] == ' ')[1] - 1, 0, na.rm = TRUE)
+    )
+  ))
 }
+date_parser <- column_transformation(.date_parser)
 
