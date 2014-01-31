389b44bdf3f0dad61c1f6d5b7cb39e62a0208d98
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-01-31 13:49:35 -0600

    select_rows_fix

--- R/select_rows.r
+++ R/select_rows.r
@@ -8,10 +8,12 @@
 #' select_rows(iris, c(T,F)) # Select only odd rows
 #' iris2 <- iris; rownames(iris2) <- paste0("row", 1:nrow(iris2))
 #' select_rows(iris, c("row10", "row51") # Select rows by name
-select_rows <- function(dataframe, rows) {
+select_rows <- function(dataframe, rows, whole = FALSE) {
   force(rows)
   eval(substitute({
-    dataframe <- dataframe[if(is.function(rows)) rows(dataframe) else rows, ]
+    dataframe <- dataframe[if(is.function(rows)) {
+      if (whole) rows(dataframe) else apply(dataframe, 1, rows)
+    } else rows, ]
   }), envir = parent.frame())
 }
 
