88c253b3b0139b8804247deaae0bdf560e456eb8
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-02-03 21:07:44 -0600

    better unit tests for numeric_to_factor

--- R/numeric_to_factor.r
+++ R/numeric_to_factor.r
@@ -5,54 +5,11 @@
 #' @param num a numeric atomic vector. Will be restored to a factor variable.
 #' @param levs a character vector of levels. 
 #' @param na.to.missing a logical. Whether to convert NAs to a "Missing" level.
-#' @export
 #' @keywords internal
 numeric_to_factor <- function(num, levs, na.to.missing = TRUE) {
-  .Call('mungebitsTransformations_numeric_to_factor',
+  res <- .Call('mungebitsTransformations_numeric_to_factor',
          num, levs, na.to.missing, PACKAGE = 'mungebitsTransformations')
-}
-
-numeric_to_factor2 <- function(num, levs, na.to.missing = TRUE) {
-  if (length(levs) == 0) stop('Zero levels provided')
-
-  levs <- as.character(levs)
-  original_levs <- levs
-
-  levs <- str_replace_all(levs, ' ', '')
-  levs <- strsplit(levs, ',')
-  numeric_levs <- vapply(levs,
-    function(lev) length(lev) == 1 &&
-      all(strsplit(lev, '')[[1]]  %in% strsplit('-.0123456789', '')[[1]]),
-    logical(1))
-
-  charnums <- character(length(num))
-  already_numeric <- as.character(num) %in% levs[numeric_levs]
-  charnums[already_numeric] <- num[already_numeric]
-
-  range_levs <- vapply(levs, function(lev) length(lev) == 2, logical(1))
-  levs[range_levs] <- lapply(levs[range_levs],
-    function(lev) {
-      left_bound <- as.numeric(substr(lev[1], 2, nchar(lev[1])))
-      right_bound <- as.numeric(substr(lev[2], 1, nchar(lev[2]) - 1))
-      left_operator <- if (substr(lev[1], 1, 1) == '(') `<` else `<=`
-      right_operator <- if (substr(lev[2], tmp <- nchar(lev[2]), tmp) == ')') `>` else `>=`
-      list(function(x) left_operator(left_bound, x),
-           function(x) right_operator(right_bound, x))
-    })
-
-  in_levs <- lapply(levs[range_levs], function(lev) {
-    lev[[1]](num[!already_numeric]) & lev[[2]](num[!already_numeric])
-  })
-  in_levs <- apply(data.frame(in_levs), 1, function(ins) which(c(ins, TRUE))[1])
-
-  charnums[!already_numeric] <- c(original_levs[range_levs], NA)[in_levs]
-
-  final_levs <- original_levs
-  if (na.to.missing && sum(is.na(charnums)) > 0)
-    final_levs <- union(original_levs, 'Missing')
-
-  if (na.to.missing) charnums[is.na(charnums)] <- 'Missing'
-
-  factor(charnums, levels = final_levs)
+  if (na.to.missing && 'Missing' %in% res) levs <- union(levs, 'Missing')
+  factor(res, levs) 
 }
 
--- inst/tests/test-numeric_to_factor.r
+++ inst/tests/test-numeric_to_factor.r
@@ -6,7 +6,7 @@ test_that("it correctly restores a bunch of levels", {
   converts <- numeric_to_factor(nums, levs)
 
   actual_restores <- 
-    c("Missing", "Missing", "Missing", "Missing", "Missing", "Missing",
+   factor(c("Missing", "Missing", "Missing", "Missing", "Missing", "Missing",
       "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]",
       "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]",
       "Missing", "Missing", "Missing", "Missing", "[-5, -3)", "[-5, -3)",
@@ -14,7 +14,29 @@ test_that("it correctly restores a bunch of levels", {
       "[2, 10)", "[2, 10)", "[2, 10)", "[2, 10)", "[2, 10)", "[2, 10)",
       "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)",
       "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)", "Missing", "Missing",
-      "Missing", "Missing", "Missing", "Missing")
+      "Missing", "Missing", "Missing", "Missing"),
+    levels = c("[2, 10)", "[10, 20)", "[-5, -3)", "(-20, -10]", "[0, 1]", "-2", "Missing"))
 
   expect_equal(converts, actual_restores)
 })
+
+test_that("it correctly restores a bunch of levels without adding Missing", {
+  nums <- -25:25
+  levs <- c("[2, 10)", "[10, 20)", "[-5, -3)", "(-20, -10]", "[0, 1]", "-2")
+  converts <- numeric_to_factor(nums, levs, FALSE)
+
+  actual_restores <- 
+   factor(c(NA, NA, NA, NA, NA, NA,
+      "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]",
+      "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]", "(-20, -10]",
+      NA, NA, NA, NA, "[-5, -3)", "[-5, -3)",
+      NA, "-2", NA, "[0, 1]", "[0, 1]", "[2, 10)", "[2, 10)",
+      "[2, 10)", "[2, 10)", "[2, 10)", "[2, 10)", "[2, 10)", "[2, 10)",
+      "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)",
+      "[10, 20)", "[10, 20)", "[10, 20)", "[10, 20)", NA, NA,
+      NA, NA, NA, NA),
+    levels = c("[2, 10)", "[10, 20)", "[-5, -3)", "(-20, -10]", "[0, 1]", "-2"))
+
+  expect_equal(converts, actual_restores)
+})
+
