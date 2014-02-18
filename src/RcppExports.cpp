437e372cd3e4179ade2844d10fccef707c36dbb3
RobertZK <technoguyrob@gmail.com>
2014-02-17 19:15:03 -0600

    reverse this

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,6 +2,9 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include <Rcpp.h>
+#include <algorithm>
+#include <string>
+#include <vector>
 
 using namespace Rcpp;
 
@@ -12,9 +15,9 @@ BEGIN_RCPP
     SEXP __sexp_result;
     {
         Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< NumericVector >::type num(numSEXP );
-        Rcpp::traits::input_parameter< CharacterVector >::type levs(levsSEXP );
-        Rcpp::traits::input_parameter< bool >::type na_to_missing(na_to_missingSEXP );
+        NumericVector num = Rcpp::as<NumericVector >(numSEXP);
+        CharacterVector levs = Rcpp::as<CharacterVector >(levsSEXP);
+        bool na_to_missing = Rcpp::as<bool >(na_to_missingSEXP);
         CharacterVector __result = numeric_to_factor(num, levs, na_to_missing);
         PROTECT(__sexp_result = Rcpp::wrap(__result));
     }
@@ -22,3 +25,20 @@ BEGIN_RCPP
     return __sexp_result;
 END_RCPP
 }
+
+ /*
+// rcpp_hello_world
+List rcpp_hello_world();
+RcppExport SEXP mungebitsTransformations_rcpp_hello_world() {
+BEGIN_RCPP
+    SEXP __sexp_result;
+    {
+        Rcpp::RNGScope __rngScope;
+        List __result = rcpp_hello_world();
+        PROTECT(__sexp_result = Rcpp::wrap(__result));
+    }
+    UNPROTECT(1);
+    return __sexp_result;
+END_RCPP
+}
+*/
