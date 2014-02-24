de82ce521e60c20965486cbb54dba4039bb8c42f
RobertZK <technoguyrob@gmail.com>
2014-02-24 12:20:02 -0600

    force this param

--- R/column_transformation.r
+++ R/column_transformation.r
@@ -34,6 +34,7 @@ column_transformation <- function(transformation, mutating = FALSE, named = FALS
 
     # During prediction, always use same column names as during training
     # TODO: Clean this up
+    force(input_cols)
     standard_cols <- standard_column_format(input_cols, dataframe)
 
     invisible(eval(substitute({
