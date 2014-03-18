c3921482ee0dce51d05cee21cb8f4d1849356bfd
Tong <tong.lu@avantcredit.com>
2014-03-18 18:11:43 -0500

    Update select_rows.r

--- R/select_rows.r
+++ R/select_rows.r
@@ -17,7 +17,7 @@
 #' iris2 <- iris; rownames(iris2) <- paste0("row", 1:nrow(iris2))
 #' select_rows(iris, c("row10", "row51") # Select rows by name
 select_rows <- function(dataframe, rows, whole = FALSE) {
-  force(rows)
+  force(rows); force(whole)
   eval(substitute({
     dataframe <- dataframe[if(is.function(rows)) {
       if (whole) rows(dataframe) else apply(dataframe, 1, rows)
