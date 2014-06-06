cdb72569aa1044dcf9263bc71ee2688620f3ec5d
Ryland Ely <ryland.ely@avantcredit.com>
2014-06-06 10:00:19 -0500

    added upsampling function & test

---
+++ R/sampler.r
@@ -0,0 +1,16 @@
+#' Take a data frame and upsample a single segment
+#' i.e. Given a variable var and a value val, randomly sample with replacement the rows with var == val
+#'
+#' @param df a data frame
+#' @export
+sampler <- function(df, var, val, frac) {
+  eval(substitute({
+    rows <- which(df[[var]]==val) 
+    n <- length(rows)
+    n2 <- round(frac*n)
+    rows_to_keep = sample(rows, n2, replace=T)
+    the_rows <- df[rows_to_keep,]
+    the_rest <- df[df[[var]]!=val,]
+    df <- rbind(the_rows, the_rest)
+  }), envir = parent.frame())
+}
