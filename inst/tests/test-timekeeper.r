6b6fb6a30ad015bfa4cf41f02373e32ff0b82a90
Peter Hurford <peter@peterhurford.com>
2014-09-30 15:09:42 -0500

    test for YYYY/MM/D

--- inst/tests/test-timekeeper.r
+++ inst/tests/test-timekeeper.r
@@ -8,6 +8,14 @@ test_that("it converts YYYY-MM-DD to date", {
   expect_equal(as.Date("1991-12-11"), mp$data$x)
 })
 
+test_that("it converts YYYY/MM/DD to date", {
+  df <- data.frame(x='1991/12/11', y='blah')
+  mp <- mungebits:::mungeplane(df)
+  mb <- mungebits:::mungebit(timekeeper)
+  mb$run(mp, 1)
+  expect_equal(as.Date("1991/12/11"), mp$data$x)
+})
+
 test_that("it converts to numeric date", {
   df <- data.frame(x='1991-12-11', y='blah')
   mp <- mungebits:::mungeplane(df)
