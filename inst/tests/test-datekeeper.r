22780d810c79fc13dfbb8bd844962f39576791eb
Peter Hurford <peter@peterhurford.com>
2014-10-02 16:46:18 -0500

    DRY tests

--- inst/tests/test-datekeeper.r
+++ inst/tests/test-datekeeper.r
@@ -1,194 +1,35 @@
 context("datekeeper")
 library(timeDate)
 
-test_that("it converts YYYY-MM-DD to date", {
-  df <- data.frame(x='1991-12-11', y='blah')
+check_date <- function(date, expectation, mode="date") {
+  df <- data.frame(x = date, y = 'bleh')
   mp <- mungebits:::mungeplane(df)
   mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991-12-11'), mp$data$x)
-})
+  mb$run(mp, 1, mode=mode)
+  eval(substitute(expectation))
+}
 
-test_that("it converts YYYY/MM/DD to date", {
-  df <- data.frame(x='1991/12/11', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts YYYY MM DD to date", {
-  df <- data.frame(x='1991 12 11', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts MM-DD-YYYY to date", {
-  df <- data.frame(x='12-11-1991', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts MM-DD-YY to date (20th century)", {
-  df <- data.frame(x='12-11-91', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts MM-DD-YY to date (21st century)", {
-  df <- data.frame(x='10-01-14', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('2014/10/01'), mp$data$x)
-})
-
-test_that("it converts YYYY [Short Written Month] DD to date", {
-  df <- data.frame(x='1991 Dec 11', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts YYYY [Long Written Month] DD to date", {
-  df <- data.frame(x='1991 December 11', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts [Short Written Month] DD YYYY to date", {
-  df <- data.frame(x='Dec 11 1991', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts [Short Written Month] DD YY to date", {
-  df <- data.frame(x='Dec 11 91', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts DD [Short Written Month] YYYY to date", {
-  df <- data.frame(x='11 Dec 1991', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts DD [Short Written Month] YY to date", {
-  df <- data.frame(x='11 Dec 91', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it handles punctuation", {
-  df <- data.frame(x="December 11, '91", y="blah")
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts 1000 to date", {
-  df <- data.frame(x=1000, y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1972/09/27'), mp$data$x)
-})
-
-test_that("it converts 19911211 to the proper date", {
-  df <- data.frame(x=19911211, y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts 1991dec11 to the proper date", {
-  df <- data.frame(x='1991dec11', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts 11dec1991 to the proper date", {
-  df <- data.frame(x='11dec1991', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts dec111991 to the proper date", {
-  df <- data.frame(x='dec111991', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal(as.Date('1991/12/11'), mp$data$x)
-})
-
-test_that("it converts garbage to NA", {
-  df <- data.frame(x='garbage', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1)
-  expect_equal('NA', mp$data$x)
-})
-
-test_that("it converts to numeric date", {
-  df <- data.frame(x='1991-12-11', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1, mode="numeric")
-  expect_equal(8014, mp$data$x)
-})
-
-test_that("it converts to TRUE in is weekend mode if weekend", {
-  df <- data.frame(x='2014-10-04', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1, mode="weekend")
-  expect_equal(TRUE, mp$data$x)
-})
-
-test_that("it converts to FALSE in is weekend mode if not weekend", {
-  df <- data.frame(x='2014-09-30', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1, mode="weekend")
-  expect_equal(FALSE, mp$data$x)
-})
-
-test_that("it converts to TRUE in is holiday mode if holiday", {
-  df <- data.frame(x='2012-11-22', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1, mode="holiday")
-  expect_equal(TRUE, mp$data$x)
-})
-
-test_that("it converts to FALSE in is holiday mode if not holiday", {
-  df <- data.frame(x='2012-11-21', y='blah')
-  mp <- mungebits:::mungeplane(df)
-  mb <- mungebits:::mungebit(datekeeper)
-  mb$run(mp, 1, mode="holiday")
-  expect_equal(FALSE, mp$data$x)
-})
+test_that("it converts YYYY-MM-DD to date", { check_date('1991-12-11', expect_equal(as.Date('1991-12-11'), mp$data$x)) })
+test_that("it converts YYYY/MM/DD to date", { check_date('1991/12/11', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts YYYY MM DD to date", { check_date('1991 12 11', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts MM-DD-YYYY to date", { check_date('12-11-1991', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts MM-DD-YY to date (20th century)", { check_date('12-11-91', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts MM-DD-YY to date (21st century)", { check_date('10-01-14', expect_equal(as.Date('2014/10/01'), mp$data$x)) })
+test_that("it converts YYYY [Short Written Month] DD to date", { check_date('1991 Dec 11', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts YYYY [Long Written Month] DD to date", { check_date('1991 December 11', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts [Short Written Month] DD YYYY to date", { check_date('Dec 11 1991', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts [Short Written Month] DD YY to date", { check_date('Dec 11 91', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts DD [Short Written Month] YYYY to date", { check_date('11 Dec 1991', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts DD [Short Written Month] YY to date", { check_date('11 Dec 91', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it handles punctuation", { check_date("December 11, '91", expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts 1000 to date", { check_date(1000, expect_equal(as.Date('1972/09/27'), mp$data$x)) })
+test_that("it converts 19911211 to the proper date", { check_date(19911211, expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts 1991dec11 to the proper date", { check_date('1991dec11', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts 11dec1991 to the proper date", { check_date('11dec1991', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts dec111991 to the proper date", { check_date('dec111991', expect_equal(as.Date('1991/12/11'), mp$data$x)) })
+test_that("it converts garbage to NA", { check_date('garbage', expect_equal('NA', mp$data$x)) })
+test_that("it converts to numeric date", { check_date('1991-12-11', expect_equal(8014, mp$data$x), 'numeric') })
+test_that("it converts to TRUE in is weekend mode if weekend", { check_date('2014-10-04', expect_equal(TRUE, mp$data$x), 'weekend') })
+test_that("it converts to FALSE in is weekend mode if not weekend", { check_date('2014-09-30', expect_equal(FALSE, mp$data$x), 'weekend') })
+test_that("it converts to TRUE in is holiday mode if holiday", { check_date('2012-11-22', expect_equal(TRUE, mp$data$x), 'holiday') })
+test_that("it converts to FALSE in is holiday mode if not holiday", { check_date('2012-11-21', expect_equal(FALSE, mp$data$x), 'holiday') })
