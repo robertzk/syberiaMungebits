3a2d9745b80512832a9d7ee2aed333074d16d87c
Peter Hurford <peter@peterhurford.com>
2014-10-01 19:19:13 -0500

    Reorder functions

--- R/datekeeper.r
+++ R/datekeeper.r
@@ -16,104 +16,8 @@
 #'   "holiday" will return TRUE if it's a US federal holiday and FALSE if not
 #'   "weekend" will return TRUE if it's a weekend and FALSE if not
 #'   "businessday" will return TRUE if it's a weekend or holiday
-
-is.weekend <- function(date) {
-  weekdays(date) == 'Saturday' || weekdays(date) == 'Sunday'
-}
-
-is.holiday <- function(date) {
-  year = as.numeric(format(date, '%Y'))
-  if (date == as.Date(ChristmasDay(year))) { return(TRUE) }
-  if (date == as.Date(USNewYearsDay(year))) { return(TRUE) }
-  if (date == as.Date(USMemorialDay(year))) { return(TRUE) }
-  if (date == as.Date(USIndependenceDay(year))) { return(TRUE) }
-  if (date == as.Date(LaborDay(year))) { return(TRUE) }
-  if (date == as.Date(USThanksgivingDay(year))) { return(TRUE) }
-  FALSE
-}
-
-handle_two_digit_years <- function(input) {
-  strips = strsplit(input, "-")
-  if (length(strips[[1]]) != 3) { return("NA") }
-  for (strip in strips[[1]]) {
-    if (nchar(strip) == 4) { return(input) }
-  }
-  if (as.numeric(strips[[1]][3]) > 20) { year_beginning = 19 }
-  else { year_beginning = 20 }
-  paste0(strips[[1]][1], '-', strips[[1]][2], '-', year_beginning, strips[[1]][3])
-}
-
-handle_order <- function(input) {
-  strips = strsplit(input, "-")
-  if (nchar(strips[[1]][3]) == 4) { output = paste0(strips[[1]][3], "-", strips[[1]][1], "-", strips[[1]][2]) }
-  else { output = input }
-  output
-}
-
-handle_month <- function(input) {
-  if (is.character(input) && input != "NA") {
-    months = c('jan', 'feb', 'mar', 'apr', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
-    strips = strsplit(input, "-")
-    if (length(strips[[1]]) != 3) { return("NA") }
-    for (month in months) {
-      if (grepl(month, strips[[1]][2])) { input = as.Date(input, '%Y-%B-%d') }
-      if (grepl(month, strips[[1]][3])) { input = as.Date(input, '%Y-%d-%B') }
-    }
-  }
-  input
-}
-
-standardize_dividers <- function(input) {
-  output = tolower(input)
-  output = gsub(' ', '-', output)
-  output = gsub('\\/', '-', output)
-  output = gsub('([a-z])(?=[0-9])', '\\1-', output, perl=T)
-  output = gsub('([0-9])(?=[a-z])', '\\1-', output, perl=T)
-
-  splits = strsplit(output, "-")
-  if (length(splits[[1]]) == 2 && nchar(splits[[1]][2]) == 6) {
-    output = paste0(splits[[1]][1], '-', substr(splits[[1]][2],0,2), '-', substr(splits[[1]][2],3,7))
-  }
-
-  output
-}
-
-remove_punctuation <- function(input) {
-  gsub('[^a-zA-Z0-9-]', '', input)
-}
-
-handle_numeric <- function(input) {
-  if (nchar(input) != 8) { output = as.Date(input, origin='1970/1/1') }
-  else {
-    output = as.Date(paste0(substring(input,0,4), '-', substring(input,5,6), '-', substring(input,7,8)))
-  }
-  output
-}
-
-convert_incoming <- function(input) {
-  # Standardize from many inputs
-  input = input[[1]]
-  date = tryCatch({
-    if (is.numeric(input)) { handle_numeric(input) }
-    else if (is.character(input)) { as.Date(input) }
-    else if (class(input) == 'Date') { input }
-    else { "NA" }
-  }, error = function(cond) { return("NA") })
-  date
-}
-
-convert_outgoing <- function(input, mode="date") {
-  # Pipe to many outputs
-  output = "NA"
-  if (as.character(input) != "NA") {
-    if (mode == "numeric") { output = as.numeric(input) }
-    else if (mode == "holiday") { output = is.holiday(input) }
-    else if (mode == "weekend") { output = is.weekend(input) }
-    else if (mode == "businessday") { output = !is.holiday(input) && !is.weekend(input) }
-    else { output = input }
-  }
-  output
-}
+#' @export
+datekeeper <- column_transformation(datekeeper_fn, mutating = TRUE, named = TRUE)
 
 datekeeper_fn <- function(input, mode="date") {
   input = input[[1]]
@@ -133,5 +37,96 @@ datekeeper_fn <- function(input, mode="date") {
   }
 }
 
-#' @export
-datekeeper <- column_transformation(datekeeper_fn, mutating = TRUE, named = TRUE)
+standardize_dividers <- function(input) {
+  output = tolower(input)
+  output = gsub(' ', '-', output)
+  output = gsub('\\/', '-', output)
+  output = gsub('([a-z])(?=[0-9])', '\\1-', output, perl=T)
+  output = gsub('([0-9])(?=[a-z])', '\\1-', output, perl=T)
+  splits = strsplit(output, "-")
+  if (length(splits[[1]]) == 2 && nchar(splits[[1]][2]) == 6) {
+    output = paste0(splits[[1]][1], '-', substr(splits[[1]][2],0,2), '-', substr(splits[[1]][2],3,7))
+  }
+  output
+}
+
+remove_punctuation <- function(input) {
+  gsub('[^a-zA-Z0-9-]', '', input)
+}
+
+handle_order <- function(input) {
+  strips = strsplit(input, "-")
+  if (nchar(strips[[1]][3]) == 4) { output = paste0(strips[[1]][3], "-", strips[[1]][1], "-", strips[[1]][2]) }
+  else { output = input }
+  output
+}
+
+handle_two_digit_years <- function(input) {
+  strips = strsplit(input, "-")
+  if (length(strips[[1]]) != 3) { return("NA") }
+  for (strip in strips[[1]]) {
+    if (nchar(strip) == 4) { return(input) }
+  }
+  if (as.numeric(strips[[1]][3]) > 20) { year_beginning = 19 }
+  else { year_beginning = 20 }
+  paste0(strips[[1]][1], '-', strips[[1]][2], '-', year_beginning, strips[[1]][3])
+}
+
+handle_month <- function(input) {
+  if (is.character(input) && input != "NA") {
+    months = c('jan', 'feb', 'mar', 'apr', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
+    strips = strsplit(input, "-")
+    if (length(strips[[1]]) != 3) { return("NA") }
+    for (month in months) {
+      if (grepl(month, strips[[1]][2])) { input = as.Date(input, '%Y-%B-%d') }
+      if (grepl(month, strips[[1]][3])) { input = as.Date(input, '%Y-%d-%B') }
+    }
+  }
+  input
+}
+
+convert_incoming <- function(input) {
+  input = input[[1]]
+  date = tryCatch({
+    if (is.numeric(input)) { handle_numeric(input) }
+    else if (is.character(input)) { as.Date(input) }
+    else if (class(input) == 'Date') { input }
+    else { "NA" }
+  }, error = function(cond) { return("NA") })
+  date
+}
+
+convert_outgoing <- function(input, mode="date") {
+  output = "NA"
+  if (as.character(input) != "NA") {
+    if (mode == "numeric") { output = as.numeric(input) }
+    else if (mode == "holiday") { output = is.holiday(input) }
+    else if (mode == "weekend") { output = is.weekend(input) }
+    else if (mode == "businessday") { output = !is.holiday(input) && !is.weekend(input) }
+    else { output = input }
+  }
+  output
+}
+
+is.weekend <- function(date) {
+  weekdays(date) == 'Saturday' || weekdays(date) == 'Sunday'
+}
+
+is.holiday <- function(date) {
+  year = as.numeric(format(date, '%Y'))
+  if (date == as.Date(ChristmasDay(year))) { return(TRUE) }
+  if (date == as.Date(USNewYearsDay(year))) { return(TRUE) }
+  if (date == as.Date(USMemorialDay(year))) { return(TRUE) }
+  if (date == as.Date(USIndependenceDay(year))) { return(TRUE) }
+  if (date == as.Date(LaborDay(year))) { return(TRUE) }
+  if (date == as.Date(USThanksgivingDay(year))) { return(TRUE) }
+  FALSE
+}
+
+handle_numeric <- function(input) {
+  if (nchar(input) != 8) { output = as.Date(input, origin='1970/1/1') }
+  else {
+    output = as.Date(paste0(substring(input,0,4), '-', substring(input,5,6), '-', substring(input,7,8)))
+  }
+  output
+}
