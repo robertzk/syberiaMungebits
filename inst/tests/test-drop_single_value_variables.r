8f510b07c2d20a54ade0c15502bdee59ae54dbb6
Peter Hurford <peter@peterhurford.com>
2014-10-03 17:34:41 -0500

    simpler drop single value variables test

--- inst/tests/test-drop_single_value_variables.r
+++ inst/tests/test-drop_single_value_variables.r
@@ -1,30 +1,18 @@
 context("drop_single_value_variables_fn")
 
-test_that("it correctly drops a 1-value column", {
+run_test <- function(switch=0) {
   mungebits_loaded <- 'mungebits' %in% loadedNamespaces(); require(mungebits)
-  dataframe <- data.frame(cbind(rep(1, 1000), rep(c(1,2), 500)))
+  if (switch==0) { dataframe <- data.frame(cbind(rep(1, 1000), rep(c(1,2), 500))) }
+  else { dataframe <- data.frame(cbind(rep(c(1,2), 500), rep(c(1,2), 500))) }
   mb <- mungebits:::mungebit(drop_single_value_variables)
   mp <- mungebits:::mungeplane(dataframe)
   mb$run(mp)
+  if (switch==1) { expect_equal(mp$data, dataframe) }
   dataframe <- data.frame(cbind(rep(c(1,2), 500), rep(c(1,2), 500)))
   mp2 <- mungebits:::mungeplane(dataframe)
   mb$run(mp2)
-  expect_equal(mp$data, mp2$data)
+  if (switch==0) { expect_equal(mp$data, mp2$data) }
   if (!mungebits_loaded) unloadNamespace('mungebits')
-})
-
-test_that("it correctly keeps a 2-value column", {
-  mungebits_loaded <- 'mungebits' %in% loadedNamespaces(); require(mungebits)
-  dataframe <- data.frame(cbind(rep(c(1,2), 500), rep(c(1,2), 500)))
-  mb <- mungebits:::mungebit(drop_single_value_variables)
-  mp <- mungebits:::mungeplane(dataframe)
-  mb$run(mp)
-  expect_equal(mp$data, dataframe)
-  dataframe <- data.frame(cbind(rep(c(1,2), 500), rep(c(1,2), 500)))
-  mp2 <- mungebits:::mungeplane(dataframe)
-  mb$run(mp2)
-  expect_equal(mp$data, dataframe)
-  if (!mungebits_loaded) unloadNamespace('mungebits')
-})
-
-
+}
+test_that("it correctly drops a 1-value column", { run_test(0) })
+test_that("it correctly keeps a 2-value column", { run_test(1) })
