9659051088fe1cc1406cbbb1ee0cc47a8caed0d6
iveksl2 <igor.veksler@avantcredit.com>
2014-10-27 15:10:32 -0500

    threshold has default argument

--- R/remove_outliers.r
+++ R/remove_outliers.r
@@ -3,10 +3,10 @@
 #' @param x an numeric vector
 #' @param threshold a z_score threshold from which the absolute value of a z_score above this threshold will be set to NA 
 
-remove_outliers_fn <- function(x, threshold ) {
+remove_outliers_fn <- function(x, threshold = 3) {
     if (!'removed_outliers' %in% names(inputs)) {
-      inputs$mean <- mean(x, is.na=T)
-      inputs$sd <- sd(x, is.na=T) 
+      inputs$mean <- mean(x, is.na=TRUE)
+      inputs$sd <- sd(x, is.na=TRUE) 
       inputs$removed_outliers <- TRUE  ## not sure if another mungebit has a mean namespace 
     }
     x[abs((x - inputs$mean(x)) / inputs$sd) > threshold] <- NA
@@ -14,5 +14,5 @@ remove_outliers_fn <- function(x, threshold ) {
 }
 
 #' @export
-remove_outliers <- column_transformation(remove_outliers_fn )
+remove_outliers <- column_transformation(remove_outliers_fn)
 
