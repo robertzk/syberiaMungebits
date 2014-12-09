f92dfd693d7e19a50846d3a688ec326543160fd9
iveksl2 <igor.veksler@avantcredit.com>
2014-12-09 16:50:16 -0600

    visualization mungebit/stage

--- R/visualize.r
+++ R/visualize.r
@@ -1,14 +1,16 @@
-#' attain visualize insights into data  
-
-visualize <- function(df, ...) {
-  par(mfrow = c(2,2)) 
-  for (i in 1:ncol(df) ) {
-    if ( i %% 4 == 0 ) par(ask=TRUE) 
-    overlayed_histogram(df, hist_var = names(df)[i], ...)
-  }
-  par(ask = FALSE) 
-}
-
+#' Attain Visual insights into data. Plot overlayed histogram vs dep_var for each variable in dataframe
+#'
+#' @param dataframe a data.frame
+#' @param ... excess arguments one can pass to the overlayed_histogram function. I.E. line_var, histColor, etc.
 #' @export
-# visualize <- column_transformation(visualize, mutating = FALSE, whole = TRUE)
+visualize  <- function(dataframe, ...) {
+  par(mfrow = c(2,2)) # can make it a param if requested
+  for (i in 1:ncol(dataframe) ) {
+    if ( i %% 4 == 0 ) par(ask=TRUE) 
+    overlayed_histogram(dataframe, hist_var = names(dataframe)[i], ...)
+  }
+  # TODO -> how to force code below to run with interrupts 
+  par(ask = FALSE) 
+  par(mfrow = c(1,1))
+}
 
