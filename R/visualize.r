8ea6fbcd50525cff9bb83fa0635e0ff882fbe2a6
iveksl2 <igor.veksler@avantcredit.com>
2014-12-09 11:41:42 -0600

    wip

--- R/visualize.r
+++ R/visualize.r
@@ -1,15 +1,14 @@
 #' attain visualize insights into data  
 
-visualize <- function(df) {
-  for (i in 1:ncol(df) {
-    overlayed_histogram(df, df[,i], histColor = 'brown', lineColor = 'blue', bins = 12)       
+visualize <- function(df, ...) {
+  par(mfrow = c(2,2)) 
+  for (i in 1:ncol(df) ) {
+    if ( i %% 4 == 0 ) par(ask=TRUE) 
+    overlayed_histogram(df, hist_var = names(df)[i], ...)
   }
+  par(ask = FALSE) 
 }
 
-# Truncates Decimals after specified numers of Digits. 
-#' i.e trunc.dig(5.732 , digits = 1) => 5.7 
-# trunc.dig <- function(x, digits = 1) trunc(x * 10^digits) / 10^digits
-
 #' @export
-# truncator <- column_transformation(truncator_fn, mutating = TRUE, named = TRUE)
+# visualize <- column_transformation(visualize, mutating = FALSE, whole = TRUE)
 
