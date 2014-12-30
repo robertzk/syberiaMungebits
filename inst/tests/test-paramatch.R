113525e37fcc915ea87259bd26762c7ac0a615a3
RobertZK <technoguyrob@gmail.com>
2014-12-30 11:30:09 -0600

    fix some tests

--- inst/tests/test-paramatch.R
+++ inst/tests/test-paramatch.R
@@ -10,12 +10,13 @@ test_that("Only whole words are being counted", {
                data.frame(col_AN = c(2, 0), col_ANT = c(1, 0)))
 })
 
-test_that("Check that N defaults the the number of unique words if set to high") {
+test_that("Check that N defaults the the number of unique words if set to high", {
   expect_equal(suppressMessages(paramatch(c("goldeen goldeen goldeen", "goldeen goldeen"), 6)), 
                paramatch(c("goldeen goldeen goldeen", "goldeen goldeen"), 1))
-}
+})
 
 test_that("punctuation works in the same way that spaces do", {
   expect_equal(paramatch(c("Talking,like?Christopher...Walken*", "//Will// have# no<> effect =) . . ."), 2, suppress.input = TRUE), 
                paramatch(c("Talking like Christopher Walken", "Will have no effect"), 2, suppress.input = TRUE))
 })
+
