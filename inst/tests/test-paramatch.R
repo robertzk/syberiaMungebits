03286713cd413fd8f1aa16a9cbdaa62184c807e1
Mike Bruno <mike.bruno@avantcredit.com>
2014-12-31 15:38:45 -0600

    Updated Tests to handle the mungebit

--- inst/tests/test-paramatch.R
+++ inst/tests/test-paramatch.R
@@ -1,22 +1,69 @@
 context('paramatch')
 
-test_that("It handles blank, space only, or punctuation entries (which should all be ignored", {
-  expect_equal(paramatch(c("hello?", " ", "???", " =( "), 3, suppress.input = TRUE), 
-               data.frame(col_HELLO = c(1,0,0,0)))
+df <- data.frame(
+  id = 1:10,
+  text = c("goldeen goldeen goldeen", 
+           "goldeen goldeen",
+           "(^_^)     hello? hello? hello?",
+           " ", 
+           " O==00==O ",
+           "An ant annhililated an ardvark and another ant.", 
+           "An Ardvark's favorite meal is an ant.",
+           "Talking,like?Christopher...Walken* //Will// have# no<> effect =) . . .",
+           "Talking like Christopher Walken will have no effect",
+           "Only whole words are being counted"),
+  stringsAsFactors = FALSE
+)
+df2 <- data.frame(
+  id = 1:3,
+  text = c("This is the end",
+           "My only friend the end", 
+           "My friend put it to the end"),
+  stringsAsFactors = FALSE
+)
+test_that("The training data is perserved for use with predictions", {
+  mp <- mungebits:::mungeplane(df)
+  mb <- mungebits:::mungebit(paramatch)
+  mb$run(mp, col = 'text', top_n = 3)
+  mp2 <- mungebits:::mungeplane(df2)
+  mb$run(mp2, col = 'text', top_n = 3)
+  expect_equal(colnames(mp$data), colnames(mp2$data))
 })
-
-test_that("Only whole words are being counted", {
-  expect_equal(paramatch(c("An ant annhililated an ardvark.", "Ardvarks love eating ants."), 2, suppress.input = TRUE),
-               data.frame(col_AN = c(2, 0), col_ANT = c(1, 0)))
-})
-
+mb <- NULL
 test_that("Check that N defaults the the number of unique words if set to high", {
-  expect_equal(suppressMessages(paramatch(c("goldeen goldeen goldeen", "goldeen goldeen"), 6)), 
-               paramatch(c("goldeen goldeen goldeen", "goldeen goldeen"), 1))
+  mp <- mungebits:::mungeplane(df[1:2,])
+  mp2 <- mp
+  mb <- mungebits:::mungebit(paramatch)
+  expect_equal(mb$run(mp, col = 'text', top_n = 1),
+               suppressMessages(mb$run(mp2, col = 'text', top_n = 6)))
+})
+mb <- NULL
+test_that("It handles blank, space only, or punctuation entries (which should all be ignored)", {
+    mp <- mungebits:::mungeplane(df[3:5,])
+    mb <- mungebits:::mungebit(paramatch)
+    mb$run(mp, col = 'text', top_n = 1)
+    expect_equal(mp$data$col_HELLO, c(3,0,0))
+  })
+mb <- NULL
+test_that("Only whole words are being counted", {
+  mp <- mungebits:::mungeplane(df[6:7,])
+  mb <- mungebits:::mungebit(paramatch)
+  mb$run(mp, col = 'text', top_n = 2, suppress.input = TRUE)
+  rownames(mp$data) <- NULL
+  expect_equal(mp$data[,-1], data.frame(col_AN = c(2, 2), col_ANT = c(2, 1)))
+})
+mb <- NULL
+test_that("punctuation works in the same way that spaces do", {
+  mp <- mungebits:::mungeplane(df[8:9,])
+  mb <- mungebits:::mungebit(paramatch)
+  mb$run(mp, col = 'text', top_n = 4, suppress.input = TRUE)
+  row1 <- mp$data[1, -1]
+  row2 <- mp$data[2, -1]
+  rownames(row1) <- NULL
+  rownames(row2) <- NULL
+  expect_equal(row1, row2)
 })
 
-test_that("punctuation works in the same way that spaces do", {
-  expect_equal(paramatch(c("Talking,like?Christopher...Walken*", "//Will// have# no<> effect =) . . ."), 2, suppress.input = TRUE), 
-               paramatch(c("Talking like Christopher Walken", "Will have no effect"), 2, suppress.input = TRUE))
-})
+
+
 
