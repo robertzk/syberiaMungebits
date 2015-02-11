f40611da6328513c39fcd634f83857bf122a0de5
Mike Bruno <mike.bruno@avantcredit.com>
2015-02-10 19:18:00 -0600

    Undo last commit

--- R/paramatch.r
+++ R/paramatch.r
@@ -21,8 +21,9 @@ paramatch <- function(dataframe, col, top_n = 5, suppress.input = FALSE, blackli
   paragraph_col <- dataframe[[col]]
   # Make sure inputs are valid
   stopifnot(is.character(paragraph_col))
-  stopifnot(is.integer(top_n))
+  stopifnot(is.numeric(top_n))
   stopifnot(top_n >= 1)
+  stopifnot(top_n %% 1 == 0)
   stopifnot(is.logical(suppress.input))
   # Standardize the input
   paragraph_col <- toupper(paragraph_col)
