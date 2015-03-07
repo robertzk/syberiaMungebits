b69635a45c5ce58a05bad64b2f397434a5d45195
Peter Hurford <peter@peterhurford.com>
2015-03-07 13:02:28 -0600

    more documentation for new_variable and replace_variable

--- R/replace_variable.R
+++ R/replace_variable.R
@@ -1,6 +1,19 @@
 #' Replaces a variable in the dataframe.
+#'
 #' @param df dataframe. The dataframe to modify.
-#' @param fun function. The function to run to convert the input into the new input.
+#' @param fun function. The function to run to convert the input into the new input.  Whichever argument is passed first to the function is the variable that will be replaced.
+#' @return Does not return anything, but modifies-in-place the dataframe to replace that variable.
+#' @author Peter Hurford
+#' @examples
+#' replace_variable(iris, function(Sepal.Width) function(Sepal.Length) Sepal.Length + 1)  # Repaces Sepal.Width to add 1 to each datapoint.
+#' replace_variable(iris, function(Sepal.Length, Sepal.Width) Sepal.Width + Sepal.Length)  # Repaces Sepal.Length to add the corresponding Sepal.Width to each datapoint for Sepal.Length.
+#'
+#' Usage in a Syberia file:
+#' list("Replace Sepal.Length with Sepal.Length + 1" = list(
+#'  replace_variable,
+#'  function(Sepal.Length) Sepal.Length + 1,
+#' ))
+#'
 #' @export
 replace_variable <- function(df, fun) {
   eval.parent(substitute({
