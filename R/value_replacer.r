2f5f1990b3fd615db66565b3602c9758542c5d3d
davidboren <david.boren@avant.com>
2015-11-16 13:49:52 -0800

    add all specified new levels whether they exist or not

--- R/value_replacer.r
+++ R/value_replacer.r
@@ -14,22 +14,31 @@
 value_replacer_fn <- function(x, values_map) { 
   replaced <- x
   is_factor <- is.factor(replaced)
-  if (is_factor) replaced <- as.character(replaced)
+  if (is_factor) {
+    replaced <- as.character(replaced)
+    rep_levels <- unique(unlist(lapply(values_map, function(value_map) value_map[[2]]))) 
+  }
   unnamed_indices <- names(values_map) == ""
   if (is.null(unnamed_indices) || length(unnamed_indices) == 0)
     unnamed_indices <- TRUE
-  for (value_map in values_map[unnamed_indices]) {
-    replaced[x %in% value_map[[1]]] <-
-      if (is_factor) as.character(value_map[[2]]) else value_map[[2]]
-  }
-  for (name in names(values_map)[!unnamed_indices]) {
-    replaced[x == name] <- 
-      if (is_factor) as.character(values_map[[name]]) else values_map[[name]]
-  }
+  lapply(
+    values_map[unnamed_indices]
+    , function(value_map) {
+      replaced[x %in% value_map[[1]]] <<-
+        if (is_factor) as.character(value_map[[2]]) else value_map[[2]]
+    }
+  )
+  lapply(
+    names(values_map)[!unnamed_indices]
+    , function(name) {
+      replaced[x == name] <<- 
+        if (is_factor) as.character(values_map[[name]]) else values_map[[name]]
+    }
+  )
   if (is_factor) factor(replaced, levels =
     if (!exists('inputs')) unique(replaced)
     else if ('levels' %in% names(inputs)) inputs$levels
-    else inputs$levels <<- unique(replaced))
+    else inputs$levels <<- union(unique(replaced), rep_levels))
   else replaced
 }
 
