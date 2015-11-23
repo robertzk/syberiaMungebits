cbed4508d33f1ab1f8b433d27534dbc5ef6ae3bb
davidboren <david.boren@avant.com>
2015-11-23 09:29:12 -0800

    clean up

--- R/value_replacer.r
+++ R/value_replacer.r
@@ -14,13 +14,11 @@
 value_replacer_fn <- function(x, values_map) { 
   replaced <- x
   is_factor <- is.factor(replaced)
-  if (is_factor) {
-    replaced <- as.character(replaced)
-  }
+  if (is_factor) replaced <- as.character(replaced)
   unnamed_indices <- names(values_map) == ""
-  if (is.null(unnamed_indices) || length(unnamed_indices) == 0) {
+  if (is.null(unnamed_indices) || length(unnamed_indices) == 0)
     unnamed_indices <- TRUE
-  }
+ 
   rep_levels_unnamed <- unlist(lapply(values_map[unnamed_indices], function(value_map) value_map[[2]])) 
   rep_levels_named <- unlist(lapply(names(values_map)[!unnamed_indices], function(name) values_map[[name]])) 
   rep_levels <- unique(c(rep_levels_unnamed, rep_levels_named))
